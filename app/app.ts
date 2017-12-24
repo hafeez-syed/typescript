39eb4746e3680a0634ac02c2a624cc34b0d85f2e
Hafeez <feeziman007@hotmail.com>
2017-12-24 21:02:57 +1100

    first ts file conversion

--- app/app.ts
+++ app/app.ts
@@ -3,10 +3,18 @@
  */
 
 function startGame(): void {
+
+    let playerName: string;
+    logPlayer(playerName);
+
     // starting a new game
     var messageElement = document.getElementById('messages');
     messageElement.innerText = 'Welcome to MultiMath! Starting a new game . . .';
     console.log('Starting new game .... ');
+}
+
+function logPlayer(name: string) {
+    console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame').addEventListener('click', startGame);
--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 function startGame() {
+    var playerName;
+    logPlayer(playerName);
     var messageElement = document.getElementById('messages');
     messageElement.innerText = 'Welcome to MultiMath! Starting a new game . . .';
     console.log('Starting new game .... ');
+}
+function logPlayer(name) {
+    console.log("New game starting for player: " + name);
 }
 document.getElementById('startGame').addEventListener('click', startGame);
 //# sourceMappingURL=app.js.map
